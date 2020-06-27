02d8d05aed476a6df3f40a7c3f7a72167d426768
zhuchunyuan <719286213@qq.com>
2020-06-27 17:37:18 +0800

    login结束

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,4 +8,54 @@ $(function () {
         $('.login-box').show()
         $('.reg-box').hide()
     })
+
+    const form = layui.form
+    const layer = layui.layer
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            const pwd = $('.reg-box [name="password"]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        },
+    })
+
+
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        const inputparams = {
+            username: $('#form_reg [name="username"]').val(),
+            password: $('#form_reg [name="password"]').val(),
+        }
+        $.post('/api/reguser', inputparams, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+
+
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'post',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                localStorage.setItem('token', res.token)
+                console.log(res.token)
+                location.href = '/code/index.html'
+            }
+        })
+    })
+
 })
